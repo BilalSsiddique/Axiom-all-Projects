c7814594930c2ec603570e4cbc6bf8367d139918
BilalSsiddique <bsiddiqui@students.uit.edu>
2022-08-07 02:46:08 +0500

    Updated JavaScript and HTML
    Added seats count and price Functionality

--- project2/script.js
+++ project2/script.js
@@ -7,11 +7,28 @@ const ticketPrice = + movieSelect.value;
 // console.log(ticketPrice);
 // Eventlistner on container to reduce complexity
 
+function updateselectedcount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatNumber = document.getElementById('num');
+    const seatCount = selectedSeats.length;
+    count.innerText = seatCount;
+    total.innerText = seatCount * ticketPrice;
+
+
+    const unseats = Array()
+    for (const value of selectedSeats.values()) {
+        unseats.push(+value.innerHTML)
+    }
+    // console.log(unseats)
+    seatNumber.innerHTML = JSON.stringify(unseats, null, seatCount)
+
+};
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') &&
         !e.target.classList.contains('occupied')
     ) {
         e.target.classList.toggle('selected')
+        updateselectedcount();
     }
 })
 
