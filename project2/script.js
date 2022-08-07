a68dd12c371c1adec0ec0e432cd04d371ab11470
BilalSsiddique <bsiddiqui@students.uit.edu>
2022-08-07 20:14:58 +0500

    completed project02
    added Local storage functionality

--- project2/script.js
+++ project2/script.js
@@ -3,17 +3,21 @@ const seat = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-const ticketPrice = + movieSelect.value;
+let ticketPrice = + movieSelect.value;
+
 // console.log(ticketPrice);
 // Eventlistner on container to reduce complexity
+populateUI();
 
 function updateselectedcount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const seatNumber = document.getElementById('num');
+    const seatsindex = [...selectedSeats].map(seatt => [...seat].indexOf(seatt));
+    // console.log(seatsindex)
     const seatCount = selectedSeats.length;
     count.innerText = seatCount;
     total.innerText = seatCount * ticketPrice;
-
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsindex));
 
     const unseats = Array()
     for (const value of selectedSeats.values()) {
@@ -23,6 +27,28 @@ function updateselectedcount() {
     seatNumber.innerHTML = JSON.stringify(unseats, null, seatCount)
 
 };
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+// GEt data from Localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats != null && selectedSeats.length > 0) {
+        seat.forEach((seaat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seaat.classList.add('selected')
+            }
+        })
+    }
+    const selectedMovieindex = localStorage.getItem('selectedMovieIndex')
+    if (selectedMovieindex != null) {
+        movieSelect.selectedIndex = selectedMovieindex;
+    }
+}
+// Eventlisteners
+// 1. Implemented on container to check click on seats(not occupied)
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') &&
         !e.target.classList.contains('occupied')
@@ -32,4 +58,13 @@ container.addEventListener('click', (e) => {
     }
 })
 
+// 2. Event Listenern for movie select
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value)
+    updateselectedcount();
 
+})
+
+// Initial count and total price
+updateselectedcount();
